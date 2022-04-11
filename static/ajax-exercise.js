71d24adc60cec3223d4c728937060e50466715e3
Michelle Song <michssong@gmail.com>
2022-04-10 22:30:08 -0700

    Finished showWeather and orderMelons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,7 +4,12 @@
 
 function showFortune(evt) {
   // TODO: get the fortune and show it in the #fortune-text div
-}
+  fetch('/fortune')
+    .then (response => response.text())
+    .then (serverData => {
+      document.querySelector('#fortune-text').innerHTML = serverData;
+    });
+  }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
@@ -17,7 +22,12 @@ function showWeather(evt) {
   const zipcode = document.querySelector('#zipcode-field').value;
 
   // TODO: request weather with that URL and show the forecast in #weather-info
-}
+  fetch(`${url}?zipcode=${zipcode}`)
+    .then(response => response.json())
+    .then(jsonData => {
+      document.querySelector('#weather-info').innerHTML = jsonData.forecast;
+    });
+  }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
@@ -27,6 +37,24 @@ function orderMelons(evt) {
   evt.preventDefault();
 
   // TODO: show the result message after your form
+  const formInputs = {
+    melon_type : document.querySelector('#melon-type-field').value,
+    qty : document.querySelector('#qty-field').value
+  };
+  const url = '/order-melons.json';
+
+  fetch('/order-melons.json', {
+    method : 'POST',
+    body : JSON.stringify(formInputs),
+    headers : {
+      'Content-type' : 'application/json'
+    },
+  })
+  .then((response) => response.json())
+  .then(responseJson => {
+    document.querySelector('#order-status').innerHTML = responseJson.msg;
+  });
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
 }
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
